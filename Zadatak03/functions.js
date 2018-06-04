997b666097391b1cf549634d25e6978cde0fc935
MaturskiEIT <39279514+MaturskiEIT@users.noreply.github.com>
2018-06-04 18:50:11 +0200

    Add files via upload

---
+++ Zadatak03/functions.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $('#prikaz').click(function () {
+        var selektovan = $("#selectGradovi option:selected").text();
+        var prognozaFull = "http://www.naslovi.net/vremenska-prognoza/" + selektovan;
+        document.getElementById('prognoza').src = prognozaFull;
+        $('#prognoza').attr('src', prognozaFull);
+    });
+});
